33e9e3a2e043d7826a006b171e00f3753b9d551e
lucapolzone <lucapolzone@gmail.com>
2024-12-06 15:02:44 +0100

    feat: add API for User resource

---
+++ app/Http/Controllers/Api/UserController.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\UserResource;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $users = User::all();  // Recupera tutti gli utenti
+        $response = response()->json(UserResource::collection($users));  // Restituisce tutti gli utenti formattati con la risorsa UserResource
+        // dd($response);
+        return $response;
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    // public function store(Request $request)
+    // {
+    //     //
+    // }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        // $user = User::findOrFail($id);  // Recupera l'utente
+        $user = User::findOrFail($id);
+        $response = response()->json(new UserResource($user)); // Restituisce i dati dell'utente formattati
+        // $response = (new UserResource($user))->response();
+        // dd($response);
+        return $response;
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    // public function update(Request $request, $id)
+    // {
+    //     //
+    // }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    // public function destroy($id)
+    // {
+    //     //
+    // }
+}
